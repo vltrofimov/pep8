3de226ab4c40491a6cf90c55c8bdc2a66de0e89b
Vladimir Trofimov <vltrofimov@gmail.com>
2019-09-02 15:35:45 +0300

    --a

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 class MailWork:
-    def __init__(self,login,password,send_server,receive_server):
-        self.login=login
-        self.password=password
-        self.send_server=send_server
-        self.receive_server=receive_server
+    def __init__(self, login, password, send_server, receive_server):
+        self.login = login
+        self.password = password
+        self.send_server = send_server
+        self.receive_server = receive_server
 
-    def sendmessage(self,subject,header,message,*args):
-        self.subject=subject
-        self.header=header
-        self.message=message
-        self.args=args
+    def sendmessage(self, subject, header, message, *args):
+        self.subject = subject
+        self.header = header
+        self.message = message
+        self.args = args
 
         msg = MIMEMultipart()
         msg['From'] = self.login
@@ -23,31 +23,34 @@ class MailWork:
         msg['Subject'] = self.subject
         msg.attach(MIMEText(self.message))
 
-        ms = smtplib.SMTP(self.send_server, 465)  # identify ourselves to smtp yandex client
-        ms.ehlo()   # secure our email with tls encryption
-        ms.starttls() #re-identify ourselves as an encrypted connection
+        ms = smtplib.SMTP(self.send_server, port=587)
+        ms.ehlo()
+        ms.starttls()
         ms.ehlo()
         ms.login(self.login, self.password)
-        ms.sendmail(self.login,
-        ms, msg.as_string())
+        ms.sendmail(self.login, msg['To'], msg.as_string())
         ms.quit()
 
     def receivemessage(self):
-        mail = imaplib.IMAP4_SSL(self.receive_server)
+        mail = imaplib.IMAP4(self.receive_server, port=993)
         mail.login(self.login, self.password)
         mail.list()
         mail.select("inbox")
-        criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
+        if header:
+            criterion = '(HEADER Subject "%s")' % header
+        else:
+            criterion = 'ALL'
         result, data = mail.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
         email_message = email.message_from_string(raw_email)
+        print(email_message)
         mail.logout()
 
 
 if __name__ == '__main__':
-    mailwork=MailWork('vltrofimov32@yandex.ru','P@ssw0rd32','smtp.yandex.ru','imap.yandex.ru')
-    mailwork.sendmessage('Subject', 'None', 'Message','vltrofimov@gmail.com')
+    mailwork = MailWork('vltrofimov32@yandex.ru', 'P@ssw0rd33', 'smtp.yandex.ru', 'imap.yandex.ru')
+    mailwork.sendmessage('Subject', 'None', 'Привет, Владимир', 'vltrofimov@gmail.com')
     mailwork.receivemessage()
